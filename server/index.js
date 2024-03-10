414a0ab5fb4768c6576e882e199386b4480e2457
adarshkumar1983 <108082430+adarshkumar1983@users.noreply.github.com>
2024-03-10 17:38:06 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -3,13 +3,12 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-// const port = 5001;
+const port = 5000;
 
 // app.use(cors({
 //   origin: 'https://weather2-lime.vercel.app',
 // }));
 app.use(cors())
-app.use(express.json());
 
 
 
@@ -45,10 +44,9 @@ app.get('/weather', async (req, res) => {
     console.error('Error fetching weather data:', error.response ? error.response.data : error.message);
     res.status(500).json({ error: 'Failed to fetch weather data' });
   }
-  res.send("Express on Vercel");
+  
 });
 
-// app.listen(5000, () => {
-//   console.log("Running on port 5000.");
-// });
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
