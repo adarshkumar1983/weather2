7bda4bcc64835411dccc11b77238217e6d6ad5f5
adarshkumar1983 <108082430+adarshkumar1983@users.noreply.github.com>
2024-03-10 17:21:50 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.get('/weather', async (req, res) => {
   res.send("Express on Vercel");
 });
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
-});
+// app.listen(5000, () => {
+//   console.log("Running on port 5000.");
+// });
 module.exports = app;
